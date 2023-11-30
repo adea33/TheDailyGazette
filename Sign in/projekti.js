a7f6983cbd3f84d8466e5a1830baf37eb8e5dbde
EnkelaBucinca <149631357+EnkelaBucinca@users.noreply.github.com>
2023-11-30 17:04:25 +0100

    Add files via upload

--- Sign in/projekti.js
+++ Sign in/projekti.js
@@ -7,13 +7,35 @@ function validimi(){
     var password=document.getElementById('password').value;
     var confirmpassword=document.getElementById('confirmpassword').value;
 
-    if(name==='' || lastname==='' || 
+    if( name==='' || lastname==='' || 
     email==='' || password==='' ||
     confirmpassword===''){
         alert('Please enter all the required fields!');
         return false;
     }
-    return true;
+    else if(name.length<3){
+        alert('Shkruaj nje emer valid');
+        return false;
+    }
+    else if(lastname.length<3){
+        alert('Shkruaj nje mbiemer valid');
+        return false;
+    }
+    else if(password !==confirmpassword){
+        alert('Passwordi duhet te jete i njejte!');
+        return false;
+    }
+    else if(!(email.includes("@")) ){
+        alert('Shkruani nje email valide!');
+        return false;
+    }
+    else{
+        alert('Ju u loguat me sukses');
+        return true;
+    }
+
+   
+   
 
 
 }
