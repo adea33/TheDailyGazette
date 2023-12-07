ef52919bbe87a671c7478d0f52db82abaf7dfdbd
EnkelaBucinca <149631357+EnkelaBucinca@users.noreply.github.com>
2023-12-07 16:18:18 +0100

    Add files via upload

--- Sign in/projekti.js
+++ Sign in/projekti.js
@@ -1,38 +1,39 @@
 document.addEventListener("DOMContentLoaded");
+
 function validimi(){
+    let nRegex=/^[A-Z][a-z]{3,8}$/;
+    let eRegex=/[a-zA-Z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+let lRegex=/^[A-Z][a-z]{3,20}$/;
+let passRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d){8,}$/;
     
     var name=document.getElementById('name').value;
     var lastname=document.getElementById('lastname').value;
     var email=document.getElementById('email').value;
     var password=document.getElementById('password').value;
     var confirmpassword=document.getElementById('confirmpassword').value;
+    
 
-    if( name==='' || lastname==='' || 
-    email==='' || password==='' ||
-    confirmpassword===''){
-        alert('Please enter all the required fields!');
-        return false;
-    }
-    else if(name.length<3){
-        alert('Shkruaj nje emer valid');
-        return false;
-    }
-    else if(lastname.length<3){
-        alert('Shkruaj nje mbiemer valid');
-        return false;
-    }
-    else if(password !==confirmpassword){
-        alert('Passwordi duhet te jete i njejte!');
-        return false;
-    }
-    else if(!(email.includes("@")) ){
-        alert('Shkruani nje email valide!');
-        return false;
-    }
-    else{
-        alert('Ju u loguat me sukses');
-        return true;
-    }
+   if(!nRegex.test(name)){
+    alert("Emri nuk eshte valid");
+    return;
+   }
+   else if(!eRegex.test(email)){
+    alert("Email nuk eshte valid");
+   }
+  else if(!lRegex.test(lastname)){
+    alert("Shkruaj nje mbiemer valid");
+    return;
+   }
+   else if(!passRegex.test(password)){
+    alert("Krijo nje fjalekalim me te forte");
+    return;
+   }
+   else if(confirmpassword.trim() !== password.trim()){
+    alert("Passwordi duhet te jete i njejte");
+    return;
+   }
+
+
 
    
    
